bcb9ce2f7c818a950775d8875f36289b1ea242a5
Natalie Famula <nataliefamula@Natalies-MacBook-Pro.local>
2017-07-22 16:58:10 -0400

    file directory fixed

---
+++ blackmarketmealprep/footer.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * The template for displaying the footer*/
+?>
---
+++ blackmarketmealprep/page.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * The template for displaying the most basic structure of a page*/
+/** can also make templates for specific pages*/
+?>
