35c55df79f92417aebfbb63d3d1e127c58ac0332
Natalie Famula <nataliefamula@Natalies-MacBook-Pro.local>
2017-07-24 22:54:37 -0400

    added Arimo Regular font to header.php

--- blackmarketmealprep/header.php
+++ blackmarketmealprep/header.php
@@ -1,4 +1,25 @@
 <?php
 /**
  * The template for displaying the header*/
-?>
+?>?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<!--load fonts from google fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet">
+	<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
