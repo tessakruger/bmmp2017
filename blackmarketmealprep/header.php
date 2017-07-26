d7f3d4ea7c0dcae7d70b464cca039531aaee0fc2
clopez11 <char.lopez11@gmail.com>
2017-07-25 21:44:14 -0400

    add testimonials content to index, styles and header

--- blackmarketmealprep/header.php
+++ blackmarketmealprep/header.php
@@ -2,3 +2,16 @@
 /**
  * The template for displaying the header*/
 ?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Arimo|Bitter:700" rel="stylesheet">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/85807/jquery.flexslider.js" type="text/javascript"></script>
+	<script src="js/carousel.js" type="text/javascript"></script>
+
+	<?php wp_head(); ?>
+</head>
--- blackmarketmealprep/page.php
+++ blackmarketmealprep/page.php
@@ -2,4 +2,8 @@
 /**
  * The template for displaying the most basic structure of a page*/
 /** can also make templates for specific pages*/
-?>
+
+get_header(); ?>
+	
+<?php
+get_footer(); ?>
