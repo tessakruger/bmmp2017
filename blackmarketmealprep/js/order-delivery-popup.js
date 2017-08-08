dbc57bd9c500a18f8dfa5f91bfc9a023cc2931e5
Tessa Kruger <tessarfoto@gmail.com>
2017-08-08 03:07:03 -0400

    clean up function and remove z-index

--- blackmarketmealprep/js/order-delivery-popup.js
+++ blackmarketmealprep/js/order-delivery-popup.js
@@ -1,19 +1,17 @@
 $(function() {
-	$('#order-delivery-text').css({'opacity': 1, 'z-index': 0});
+	$('#order-delivery-text').css({'opacity': 0});
 
 	$('#order-popup').hover(
 		function(){
 		$('#order-delivery-text').stop()
 		.animate({
-			opacity: 1, top: "-75px"}, {queue: false, duration: 250})
-		.css('z-index', 99);
+			opacity: 1, top: "-75px"}, {queue: false, duration: 250});
 		},
 
 		function(){
 			$('#order-delivery-text').stop()
 			.animate({
-				opacity: 0, top: "-25px"}, {queue: false, duration: 250})
-			.css('z-index', 0);
+				opacity: 0, top: "-25px"}, {queue: false, duration: 250});
 		}
 	);
 });
